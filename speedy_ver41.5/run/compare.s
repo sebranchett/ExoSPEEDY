057eb88fbaf9cb3211d3d887c3c912a9e89780ea
sebranchett <s.e.branchett@tudelft.nl>
2020-02-14 14:08:30 +0100

    test output 3 for 3 and download artifacts

--- speedy_ver41.5/run/compare.s
+++ speedy_ver41.5/run/compare.s
@@ -10,5 +10,5 @@
 # diff ../output/exp_200/atva200_1979.grd ../output/exp_201/atva201_1979.grd
 
 # diff ../output/exp_200/atdf200_1979.txt ../output/exp_201/atdf201_1979.txt
-diff ../output/exp_200/attm200_1979.txt ../output/exp_201/attm201_1979.txt
-# diff ../output/exp_200/atva200_1979.txt ../output/exp_201/atva201_1979.txt
+# diff ../output/exp_200/attm200_1979.txt ../output/exp_201/attm201_1979.txt
+diff ../output/exp_200/atva200_1979.txt ../output/exp_201/atva201_1979.txt
