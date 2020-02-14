cb80b9004859a495296d6777ae984135cb384d13
sebranchett <s.e.branchett@tudelft.nl>
2020-02-14 12:26:04 +0100

    CI for Windows

---
+++ speedy_ver41.5/run/compare.s
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# diff ../output/exp_200/atdf200.ctl ../output/exp_201/atdf201.ctl
+diff ../output/exp_200/atdf200_1979.grd ../output/exp_201/atdf201_1979.grd
+diff ../output/exp_200/atdf200_1979.txt ../output/exp_201/atdf201_1979.txt
+diff ../output/exp_200/atgcm200.lis ../output/exp_201/atgcm201.lis
+diff ../output/exp_200/atgcm200.rst ../output/exp_201/atgcm201.rst
+# diff ../output/exp_200/attm200.ctl ../output/exp_201/attm201.ctl
+diff ../output/exp_200/attm200_1979.grd ../output/exp_201/attm201_1979.grd
+diff ../output/exp_200/attm200_1979.txt ../output/exp_201/attm201_1979.txt
+# diff ../output/exp_200/atva200.ctl ../output/exp_201/atva201.ctl
+diff ../output/exp_200/atva200_1979.grd ../output/exp_201/atva201_1979.grd
+diff ../output/exp_200/atva200_1979.txt ../output/exp_201/atva201_1979.txt
